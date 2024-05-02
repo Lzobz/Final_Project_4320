9a79f3d18dfee2f5a58fcef9a3dfbdd881dd4aed
Lzobz <zobelluke@gmail.com>
2024-05-02 13:26:49 -0500

    Added login function

--- app.py
+++ app.py
@@ -1,8 +1,28 @@
 from flask import Flask, render_template, request
+import sqlite3
+
+app = Flask(__name__)
+app.config["DEBUG"] = True
+
+def admin_check(username, password):
+    db_path = 'dbs/reservations.db'
+    conn = sqlite3.connect(db_path)
+    result = conn.execute("SELECT * FROM admins WHERE username = ? AND password = ?", (username, password)).fetchone()
+    conn.close() 
+    return result
 
 @app.route('/', methods=['GET', 'POST'])
 def main():
-    
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        if admin_check(username, password):
+            return render_template('admin.html')
+        else:
+            message = "Invalid username or password."
+        return render_template('admin-login.html', message=message)
+    else:
+        return render_template('admin-login.html')
 
 if __name__ == '__main__':
     app.run(port=5000)
